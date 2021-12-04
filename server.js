776943cdd17a4092df9b525c8e4df985cd295a8e
othonkok <45878388+othonkok@users.noreply.github.com>
2021-12-04 12:02:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ require ('dotenv/config')
 
 const app = express()
 
-mongoose.connect(process.env.DATABASE_URL,
+//mongoose.connect(process.env.DATABASE_URL,
+//mongoose.connect('mongodb://192.168.2.6/blog',
+mongoose.connect('mongodb://localhost/blog'
 { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected Successfully'))
-    .catch((err) => console.error('Not Connected'));
+    .then(() => console.log('Database Connected Successfully'))
+    .catch((err) => console.error('Database Not Connected'));
 
 app.use(express.json())
+app.use(cors())
+app.use('/posts', require('./routes/posts'))
 
 app.get('/', (req, res) => {
     res.send('Home')
 })
-app.use('/posts', require('./routes/posts'))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log('Server started on port: ' + port))
