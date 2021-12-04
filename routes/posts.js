9dcaa6ae7b901f806747767f340eaa643c0a9dd1
othonkok <45878388+othonkok@users.noreply.github.com>
2021-12-04 12:06:49 +0200

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 
-// Getting all
+// Get all Posts
 router.get('/', async (req, res) => {
   try {
     const posts = await Post.find()
@@ -12,4 +12,35 @@ router.get('/', async (req, res) => {
   }
 })
 
+//Create One
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.params.title,
+        description: req.params.description,
+        markdown: req.params.markdown
+    })
+    const savedPost = await post.save()
+    res.json(savedPost)
+})
+
+//Get One
+router.get('/:postId', async(req, res) => {
+    const post = await Post.findById(req.params.postId)
+    res.json(post)
+})
+
+//Delete One
+router.delete('/:postId', async (req, res) => {
+    const removedPost = await Post.remove({ _id: req.params.postId })
+    res.json(removedPost)
+})
+
+//Update One
+router.patch('/:postId', async(req, res) => {
+    const updatedPost = await Post.updateOne(
+        { _id: req.params.postId },
+        { $set: { title: req.params.title }}
+    )
+    res.json(updatedPost)
+})
 module.exports = router
